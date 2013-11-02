b8c44cbf944ccb06f040dd0c46a4918f42a6f40d
Comrod <icomrod@gmail.com>
2013-11-01 23:39:37 -0500

    Initial Commit

---
+++ GravitonTests/GravitonTests.m
@@ -0,0 +1,34 @@
+//
+//  GravitonTests.m
+//  GravitonTests
+//
+//  Created by Cormac Chester on 11/1/13.
+//  Copyright (c) 2013 Testman Industries. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GravitonTests : XCTestCase
+
+@end
+
+@implementation GravitonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
